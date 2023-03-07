07f3d4c6052aaa68bcab6742b944a98d98581f67
Victorien GOUDEAU <vgoudeau@sqli.com>
2023-03-07 19:09:59 +0100

    :sparkles: Add a history command

--- web-app/src/components/UserProfile/HistoryCommandUser.vue
+++ web-app/src/components/UserProfile/HistoryCommandUser.vue
@@ -1,16 +1,14 @@
 <template>
   <v-card
     id="titleSummary"
-    class="mx-auto"
     max-width="844"
+    max-height="500"
+    class="scroll"
     title="Mon historique de commandes"
   >
-    <div>
+    <div v-for="item in Allcommands" :key="item.commandNumber">
       <v-divider />
-      <HistoryCommand />
-      <v-divider />
-      <v-divider />
-      <HistoryCommand />
+      <HistoryCommand :commands="item" />
       <v-divider />
     </div>
   </v-card>
@@ -22,11 +20,58 @@ import HistoryCommand from "@/components/UserProfile/HistoryCommand/HistoryComma
 export default defineComponent({
   name: "HistoryCommandUser",
   components: { HistoryCommand },
+  data: () => ({
+    Allcommands: [
+      {
+        commandNumber: "",
+        commandPrice: 0,
+        commandAdress: "",
+        commandCity: "",
+        commandZipCode: "",
+        commandCountry: "",
+        commandDate: "",
+        commandArticles: [],
+      },
+    ],
+  }),
+  async created() {
+    await this.getAllCommands();
+  },
+  methods: {
+    async getAllCommands() {
+      const commands = await this.$axios.get(
+        "http://localhost:8080/commands/" + this.$cookies.get("userId"),
+        {
+          headers: {
+            Authorization: "Bearer " + this.$cookies.get("token"),
+          },
+        }
+      );
+      for (const command of commands.data) {
+        const date = new Date(command.date);
+        this.Allcommands.push({
+          commandNumber: command._id,
+          commandPrice: command.price,
+          commandAdress: command.adress,
+          commandCity: command.city,
+          commandZipCode: command.codePostal,
+          commandCountry: command.country,
+          commandDate: date.toLocaleDateString("fr"),
+          commandArticles: command.articles.items,
+        });
+      }
+      this.Allcommands.splice(0, 1);
+    },
+  },
 });
 </script>
 
 <style scoped>
 #titleSummary {
   text-align: center;
+}
+
+.scroll {
+  overflow-y: scroll;
 }
 </style>
